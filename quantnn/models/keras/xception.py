3e55ed87688bdba4c557cbd84a604d0c3b22b35e
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-02-26 17:53:15 +0100

    Fixed bug in xception net.

--- quantnn/models/keras/xception.py
+++ quantnn/models/keras/xception.py
@@ -165,7 +165,7 @@ class XceptionNet(keras.Model):
 
         self.concat = layers.Concatenate()
         self.out_block = keras.Sequential([
-            layers.Conv2D(n_outputs, 1, padding="valid", input_shape=(None, None, 128 + n_inputs)),
+            layers.Conv2D(n_outputs, 1, padding="valid", input_shape=(None, None, nf + n_inputs)),
             layers.BatchNormalization(),
             layers.ReLU(),
             layers.Conv2D(n_outputs, 1, padding="valid")
