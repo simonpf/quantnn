e47635e8d817436de36a0c00b52f0c20eaef5fa2
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-04-15 22:04:06 +0200

    Added scatter plot metric.

--- quantnn/metrics.py
+++ quantnn/metrics.py
@@ -3,7 +3,7 @@
 quatnn.metrics
 ==============
 
-This module define metric object that can be used to evaluate mode
+This module defines metric object that can be used to evaluate model
 performance during training.
 """
 from abc import ABC, abstractmethod, abstractproperty
@@ -15,9 +15,9 @@ from quantnn.backends import get_tensor_backend
 
 class Metric(ABC):
     """
-    The interface for metrics to compute during training.
+    The Metric abstract base class defines the basic interface for metric
+    objects.
     """
-
     @abstractproperty
     def name(self):
         """
@@ -53,6 +53,74 @@ class Metric(ABC):
                 values between metrics.
         """
 
+################################################################################
+# Scalar metrics
+################################################################################
+
+class Bias(Metric):
+    """
+    Calculates the bias (mean error).
+    """
+    def __init__(self):
+        self.error = {}
+        self.n_samples = {}
+        self._model = None
+        self.tensor_backend = None
+
+    @property
+    def name(self):
+        return "Bias"
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+
+    def process_batch(self, key, y_pred, y, cache=None):
+        if self.tensor_backend == None:
+            self.tensor_backend = get_tensor_backend(y_pred)
+        xp = self.tensor_backend
+
+        # Get deviation from mean from cache
+        # if not already computed.
+        if cache is not None and "y_mean" in cache:
+            y_mean = cache["y_mean"]
+        else:
+            y_mean = self.model.posterior_mean(y_pred=y_pred)
+        if cache is not None:
+            cache["y_mean"] = y_mean
+
+        dy = y_mean - y
+
+        # Calculate the error.
+        e = self.error.get(key, 0.0)
+        self.error[key] = e + dy.sum()
+        n = self.n_samples.get(key, 0.0)
+        self.n_samples[key] = n + xp.size(y_pred)
+
+    def reset(self):
+        self.error = {}
+        self.n_samples = {}
+
+    def get_values(self):
+        xp = self.tensor_backend
+        if xp is None:
+            return 0.0
+
+        if len(self.error) == 1:
+            e = next(iter(self.error.items()))[1]
+            n = next(iter(self.n_samples.items()))[1]
+            return xp.to_numpy(e / n)[0]
+
+        results = {}
+        for k in self.error:
+            results[k] = self.error[k] / self.n_samples[k]
+        return results
+
 class MeanSquaredError(Metric):
     """
     Mean squared error metric computed using the posterior mean.
@@ -81,19 +149,20 @@ class MeanSquaredError(Metric):
             self.tensor_backend = get_tensor_backend(y_pred)
         xp = self.tensor_backend
 
-
         # Get deviation from mean from cache
         # if not already computed.
+        if cache is not None and "y_mean" in cache:
+            y_mean = cache["y_mean"]
+        else:
+            y_mean = self.model.posterior_mean(y_pred=y_pred)
         if cache is not None:
-            if "dy" in cache:
-                dy = cache["dy_mean"]
-            else:
-                dy = self.model.posterior_mean(y_pred=y_pred) - y
-                cache["dy_mean"] = dy
+            cache["y_mean"] = y_mean
+
+        dy = y_mean - y
 
         # Calculate the squared error.
         se = self.squared_error.get(key, 0.0)
-        self.squared_error[key] = se + (cache["dy_mean"] ** 2).sum()
+        self.squared_error[key] = se + (dy ** 2).sum()
         n = self.n_samples.get(key, 0.0)
         self.n_samples[key] = n + xp.size(y_pred)
 
@@ -107,7 +176,7 @@ class MeanSquaredError(Metric):
             return 0.0
 
         if len(self.squared_error) == 1:
-            se = next(iter(self.squared_errors.items()))[1]
+            se = next(iter(self.squared_error.items()))[1]
             n = next(iter(self.n_samples.items()))[1]
             return xp.to_numpy(se / n)[0]
 
@@ -116,10 +185,14 @@ class MeanSquaredError(Metric):
             results[k] = self.squared_error[k] / self.n_samples[k]
         return results
 
+################################################################################
+# Calibration plot
+################################################################################
 
-class Calibration(Metric):
+class CalibrationPlot(Metric):
     """
-    The calibration of predicted quantiles.
+    Produces a plot of  the calibration of the predicted quantiles of a QRNN.
+    Currently only works in combination with the tensor board logger.
     """
     def __init__(self):
         self.calibration = {}
@@ -138,8 +211,8 @@ class Calibration(Metric):
 
     @model.setter
     def model(self, model):
-        self._model = model
 
+        self._model = model
 
     def process_batch(self, key, y_pred, y, cache=None):
 
@@ -177,7 +250,17 @@ class Calibration(Metric):
         self.n_samples = {}
 
     def make_calibration_plot(self, key):
+        """
+        Plots the calibration for a given target key using
+        matplotlib.
 
+        Args:
+            key: Name of the target for which to plot the
+                 calibration.
+
+        Return:
+            matplotlib Figure object containing the calibration plot.
+        """
         if not hasattr(self._model, "quantiles"):
             return None
 
@@ -204,7 +287,125 @@ class Calibration(Metric):
         return f
 
     def get_figures(self):
+        """
+        Return:
+             In the case of a single-target model directly returns the
+             matplotlib figure containing the calibration plot. In the
+             case of a multi-target model returns a dict of figures.
+        """
         figures = {k: self.make_calibration_plot(k) for k in self.calibration}
+        if len(figures) == 1:
+            return next(iter(figures.items()))[1]
+        return figures
+
+################################################################################
+# HeatMap
+################################################################################
+
+class ScatterPlot(Metric):
+    """
+    Produces a scatter plot of the posterior mean against the target value.
+    """
+    def __init__(self, bins=None, log_scale=False):
+        self.bins = bins
+        self.log_scale = log_scale
+        self.n_samples = {}
+        self._model = None
+        self.tensor_backend = None
+        self.mask = None
+
+        self.y_pred = {}
+        self.y = []
+
+    @property
+    def name(self):
+        return "ScatterPlot"
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+
+        self._model = model
+
+    def process_batch(self, key, y_pred, y, cache=None):
+
+        if self.tensor_backend == None:
+            self.tensor_backend = get_tensor_backend(y_pred)
+        xp = self.tensor_backend
+
+        # Get deviation from mean from cache
+        # if not already computed.
+        if cache is not None and "y_mean" in cache:
+            y_mean = cache["y_mean"]
+        else:
+            y_mean = self.model.posterior_mean(y_pred=y_pred)
+        if cache is not None:
+            cache["y_mean"] = y_mean
+
+        y_mean = xp.to_numpy(y_mean)
+        y = xp.to_numpy(y)
+
+        if self.mask is not None:
+            y_mean = y_mean[y.squeeze() > self.mask]
+
+        self.y_pred.setdefault(key, []).append(y_mean.ravel())
+
+        if len(self.y) < len(self.y_pred[key]):
+            if self.mask is not None:
+                y = y[y > self.mask]
+            self.y.append(y)
+
+    def reset(self):
+        self.y_pred = {}
+        self.y = []
+
+    def make_scatter_plot(self, key):
+        """
+        Plots a 2D histogram of the predictions against the
+        target values.
+
+        Args:
+            key: Name of the target for which to plot the
+                 results.
+
+        Return:
+            matplotlib Figure object containing the scatter plot.
+        """
+        xp = self.tensor_backend
+
+        y_pred = np.concatenate(self.y_pred[key])
+        y = np.concatenate(self.y)
+        img, x_edges, y_edges = np.histogram2d(y, y_pred, bins=self.bins)
+
+        plt.ioff()
+        f, ax = plt.subplots(1, 1, dpi=100)
+
+        ax.pcolormesh(x_edges, y_edges, img.T)
+        ax.plot(x_edges, y_edges, ls="--", c="grey")
+
+        ax.set_xlim([x_edges[0], x_edges[-1]])
+        ax.set_ylim([y_edges[0], y_edges[-1]])
+        ax.set_xlabel("Target value")
+        ax.set_ylabel("Posterior mean")
+
+        if self.log_scale:
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+
+        f.tight_layout()
+        return f
+
+    def get_figures(self):
+        """
+        Return:
+             In the case of a single-target model directly returns the
+             matplotlib figure containing the calibration plot. In the
+             case of a multi-target model returns a dict of figures.
+        """
+        figures = {k: self.make_scatter_plot(k) for k in self.y_pred}
         if len(figures) == 1:
             return next(iter(figures.items()))[1]
         return figures
