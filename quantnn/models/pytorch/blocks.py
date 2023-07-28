1ec04b527629d343efd3c338f4d139abd8c3c321
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-07-28 20:04:52 +0200

    Added transposed convolution block.

--- quantnn/models/pytorch/blocks.py
+++ quantnn/models/pytorch/blocks.py
@@ -38,7 +38,7 @@ class ConvBlockFactory:
             self,
             channels_in,
             channels_out=None,
-            downsample=1
+            downsample=1,
     ):
         """
         Args:
@@ -55,6 +55,9 @@ class ConvBlockFactory:
         if self.kernel_size > 1:
             padding = (self.kernel_size - 1) // 2
 
+        if not downsample:
+            downsample = 1
+
         blocks = [
             nn.Conv2d(
                 channels_in,
@@ -69,5 +72,79 @@ class ConvBlockFactory:
             blocks.append(self.norm_factory(channels_out))
         if self.activation_factory is not None:
             blocks.append(self.activation_factory())
+
+        if len(blocks) == 1:
+            return blocks[0]
+
+        return nn.Sequential(*blocks)
+
+
+class ConvTransposedBlockFactory:
+    """
+    A convolution block using transposed convolutions.
+
+    Combines transposed convolution with an optional normalization
+    layer and activation function.
+    """
+    def __init__(
+            self,
+            kernel_size=1,
+            norm_factory=None,
+            activation_factory=None
+    ):
+        """
+        Args:
+            kernel_size: The size of the convolution kernel.
+            norm_factory: A factory to produce the normalization layer that is
+                applied after the convolution.
+            activation_factory: A factory to produce the activation function
+                that applied after the convolution and the optional
+                normalization layer.
+        """
+        self.kernel_size = kernel_size
+        self.norm_factory = norm_factory
+        self.activation_factory = activation_factory
+
+    def __call__(
+            self,
+            channels_in,
+            channels_out=None,
+            downsample=1,
+    ):
+        """
+        Args:
+            channels_in: The number of channels in the block input.
+            channels_out: The number of channels in the block output. If not
+                given, will be the same as number of input channels.
+            downsample: Optional downsampling factor that will be used as the
+                the stride of the convolution operation.
+        """
+        if channels_out is None:
+            channels_out = channels_in
+
+        padding = 0
+        if self.kernel_size > 1:
+            padding = (self.kernel_size - 1) // 2
+
+        if not downsample:
+            downsample = 1
+
+        blocks = [
+            nn.ConvTranspose2d(
+                channels_in,
+                channels_out,
+                kernel_size=self.kernel_size,
+                padding=padding,
+                stride=downsample
+            )
+        ]
+
+        if self.norm_factory is not None:
+            blocks.append(self.norm_factory(channels_out))
+        if self.activation_factory is not None:
+            blocks.append(self.activation_factory())
+
+        if len(blocks) == 1:
+            return blocks[0]
 
         return nn.Sequential(*blocks)
--- test/models/pytorch/test_blocks.py
+++ test/models/pytorch/test_blocks.py
@@ -1,4 +1,7 @@
-from quantnn.models.pytorch.blocks import ConvBlockFactory
+from quantnn.models.pytorch.blocks import (
+    ConvBlockFactory,
+    ConvTransposedBlockFactory
+)
 
 import torch
 from torch import nn
@@ -9,13 +12,36 @@ def test_conv_block_factory():
     Ensures that the basic ConvBlockFactory produces a working
     block.
     """
-    block_factory = ConvBlockFactory()
+    block_factory = ConvBlockFactory(kernel_size=3)
     block = block_factory(8, 16)
     input = torch.ones(8, 8, 8, 8)
     output = block(input)
     assert output.shape == (8, 16, 8, 8)
 
     block_factory = ConvBlockFactory(
+        kernel_size=3,
+        norm_factory=nn.BatchNorm2d,
+        activation_factory=nn.GELU
+    )
+    block = block_factory(8, 16)
+    input = torch.ones(8, 8, 8, 8)
+    output = block(input)
+    assert output.shape == (8, 16, 8, 8)
+
+
+def test_conv_transposed_block_factory():
+    """
+    Ensures that the basic ConvBlockFactory produces a working
+    block.
+    """
+    block_factory = ConvTransposedBlockFactory(kernel_size=3)
+    block = block_factory(8, 16)
+    input = torch.ones(8, 8, 8, 8)
+    output = block(input)
+    assert output.shape == (8, 16, 8, 8)
+
+    block_factory = ConvTransposedBlockFactory(
+        kernel_size=3,
         norm_factory=nn.BatchNorm2d,
         activation_factory=nn.GELU
     )
