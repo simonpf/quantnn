Fix calculation of input stages.

quantnn/models/pytorch/aggregators.py
@@ -274,8 +274,6 @@ class LinearAggregatorFactory:
         Args:
             norm_factory:
         """
-        if norm_factory is None:
-            norm_factory = nn.BatchNorm2d
         self.norm_factory = norm_factory
 
     def __call__(
@@ -288,11 +286,11 @@ class LinearAggregatorFactory:
         if channels_out is None:
             channels_out = channels_in
         if self.norm_factory is not None:
+            blocks = nn.Conv2d(n_inputs * channels_in, channels_out, kernel_size=1)
+            if self.norm_factory is not None:
+                blocks.append(self.norm_factory(channels_out))
             return ConcatenateBlock(
-                nn.Sequential(
-                    nn.Conv2d(n_inputs * channels_in, channels_out, kernel_size=1),
-                    self.norm_factory(channels_out),
-                ),
+                nn.Sequential(blocks),
                 residual=residual
             )
         return ConcatenateBlock(

quantnn/models/pytorch/decoders.py
@@ -142,6 +142,59 @@ class SpatialDecoder(nn.Module):
             )
             channels_in = channels_out
 
+    def forward_w_intermediate(
+            self,
+            x: Union[torch.Tensor, List[torch.Tensor]]
+    ):
+        """
+        Same as 'forward' but also returns the intermediate activation
+        from after each stage.
+
+        Args:
+            x: The output from the encoder. This should be a single tensor
+               if no skip connections are used. If skip connections are used,
+               x should be list containing the outputs from each stage in the
+               encoder.
+
+        Return:
+            A list of tensors containing the activations after every stage
+            in the decoder.
+        """
+        if self.skip_connections:
+            if not isinstance(x, list):
+                raise ValueError(
+                    f"For a decoder with skip connections the input must "
+                    f"be a list of tensors."
+                )
+        else:
+            if isinstance(x, list):
+                x = x[-1]
+
+        activations = []
+
+        if isinstance(x, list):
+            if len(x) < self.n_stages + 1:
+                x = [None] * (self.n_stages + 1 - len(x)) + x
+
+            y = x[-1]
+            stages = self.stages
+
+            for x_skip, up, stage in zip(x[-2::-1], self.upsamplers, stages):
+                if x_skip is None:
+                    y = stage(up(y))
+                else:
+                    y = stage(torch.cat([x_skip, up(y)], dim=1))
+                activations.append(y)
+        else:
+
+            y = x
+            stages = self.stages
+            for up, stage in zip(self.upsamplers, stages):
+                y = stage(up(y))
+                activations.append(y)
+        return activations
+
+
     def forward(
             self,
             x: Union[torch.Tensor, List[torch.Tensor]]
@@ -180,7 +233,6 @@ class SpatialDecoder(nn.Module):
                 else:
                     y = stage(torch.cat([x_skip, up(y)], dim=1))
         else:
-
             y = x
             stages = self.stages
             for up, stage in zip(self.upsamplers, stages):

quantnn/models/pytorch/encoders.py
@@ -161,9 +161,9 @@ class SpatialEncoder(nn.Module):
                 "The list of downsampling factors numbers must have one "
                 "element less than the number of stages."
             )
-        # No downsampling applied in last layer.
-        downsampling_factors = [1] + downsampling_factors
 
+        # No downsampling applied in first layer.
+        downsampling_factors = [1] + downsampling_factors
 
         if stage_factory is None:
             stage_factory = SequentialStageFactory()
@@ -199,14 +199,16 @@ class SpatialEncoder(nn.Module):
                 )
             # Explicit downsampling layer.
             else:
-                self.downsamplers.append(
-                    downsampler_factory(
-                        channels_in,
-                        channels_out,
-                        f_dwn)
-                    if f_dwn > 1
-                    else None
-                )
+                if f_dwn > 1:
+                    self.downsamplers.append(
+                        downsampler_factory(
+                            channels_in,
+                            channels_out,
+                            f_dwn)
+                    )
+                else:
+                    self.downsamplers.append(None)
+
                 self.stages.append(
                     stage_factory(
                         channels_out,
@@ -243,9 +245,9 @@ class SpatialEncoder(nn.Module):
         y = x
         skips = []
         for down, stage in zip(self.downsamplers, self.stages):
-            y = stage(y)
             if down is not None:
                 y = down(y)
+            y = stage(y)
             skips.append(y)
         return skips
 
@@ -407,11 +409,11 @@ class MultiInputSpatialEncoder(SpatialEncoder):
             if downsampler_factory is None and stage_ind > len(channels) - 2:
                 raise ValueError(
                     "Stage index for input cannot exceed the number "
-                    " of stages in the encoder minus 2 if no explicity."
+                    " of stages in the encoder minus 2 if no explicit"
                     " downsampler factory is provided."
                 )
 
-            if stage_ind > 0 and downsampler_factory is None:
+            if stage_ind > first_stage and downsampler_factory is None:
                 stage_ind = stage_ind + 1
 
             self.stems[input_name] = stage_conf.stem_factory(
@@ -424,7 +426,6 @@ class MultiInputSpatialEncoder(SpatialEncoder):
 
 
         self.first_stage = first_stage
-
         first_input = self.stage_inputs[self.first_stage][0]
         del self.aggregators[first_input]
 