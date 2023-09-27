227825496a8054775ed9c4130f484ccf8422e1c0
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2023-09-27 23:51:57 +0200

    Added factory for up-convolution as used in the original U-Net architecture.

--- quantnn/models/pytorch/upsampling.py
+++ quantnn/models/pytorch/upsampling.py
@@ -11,12 +11,13 @@ class BilinearFactory:
     """
     A factory for producing bilinear upsampling layers.
     """
+
     def __call__(self, channels_in, channels_out, factor):
         if channels_in is not None and channels_out is not None:
             if channels_in != channels_out:
                 return nn.Sequential(
                     nn.Conv2d(channels_in, channels_out, kernel_size=1),
-                    nn.UpsamplingBilinear2d(scale_factor=factor)
+                    nn.UpsamplingBilinear2d(scale_factor=factor),
                 )
         return nn.UpsamplingBilinear2d(scale_factor=factor)
 
@@ -25,6 +26,7 @@ class UpsampleFactory:
     """
     A factory for torch's generic upsampling layer.
     """
+
     def __init__(self, **kwargs):
         self.kwargs = kwargs
 
@@ -33,6 +35,22 @@ class UpsampleFactory:
             if channels_in != channels_out:
                 return nn.Sequential(
                     nn.Conv2d(channels_in, channels_out, kernel_size=1),
-                    nn.Upsample(scale_factor=factor, **self.kwargs)
+                    nn.Upsample(scale_factor=factor, **self.kwargs),
                 )
         return nn.Upsample(scale_factor=factor, **self.kwargs)
+
+
+class UpConvolutionFactory:
+    """
+    Factory for up-convolution upsampling as used in the original
+    U-Net architecture.
+    """
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __call__(self, channels_in, channels_out, factor):
+        return nn.Sequential(
+            nn.Upsample(scale_factor=factor, **self.kwargs),
+            nn.Conv2d(channels_in, channels_out, kernel_size=factor, padding="same"),
+        )
--- test/models/pytorch/test_upsampling.py
+++ test/models/pytorch/test_upsampling.py
@@ -6,7 +6,8 @@ from torch import nn
 
 from quantnn.models.pytorch.upsampling import (
     BilinearFactory,
-    UpsampleFactory
+    UpsampleFactory,
+    UpConvolutionFactory,
 )
 
 
@@ -30,6 +31,21 @@ def test_upsample():
     factors along different dimensions.
     """
     fac = UpsampleFactory(mode="nearest")
+    up = fac(channels_in=16, channels_out=8, factor=(2, 4))
+    x = torch.rand(2, 16, 32, 32)
+    y = up(x)
+
+    assert y.shape[1] == 8
+    assert y.shape[-2] == 64
+    assert y.shape[-1] == 128
+
+
+def test_upconvolution():
+    """
+    Ensure that the up-convolution factory supports upsampling with different
+    factors along different dimensions.
+    """
+    fac = UpConvolutionFactory(mode="nearest")
     up = fac(channels_in=16, channels_out=8, factor=(2, 4))
     x = torch.rand(2, 16, 32, 32)
     y = up(x)
