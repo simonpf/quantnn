Fix scaling of conv outputs.

quantnn/models/pytorch/masked.py
@@ -62,8 +62,7 @@ def masked_conv_2d(
     )
 
     mask_new = masked_frac >= 1.0
-    #y = y / torch.where(mask_new, 1.0, (1.0 - masked_frac))
-    y = y / (1.0 - masked_frac)
+    y = y / torch.where(mask_new, 1.0, (1.0 - masked_frac))
     if bias is not None:
         y = y + bias[None, :, None, None]
 
@@ -376,10 +375,10 @@ class Linear(nn.Linear):
         x_m = torch.where(mask, 0.0, x.strip())
         y = super().forward(x_m)
 
-        valid_frac = self.counter(mask.to(dtype=x.dtype))
-        mask_new = valid_frac >= 1.0
+        masked_frac = self.counter(mask.to(dtype=x.dtype))
+        mask_new = masked_frac >= 1.0
 
-        y = y / (1.0 - valid_frac)
+        y = y / torch.where(mask_new, 1.0, (1.0 - masked_frac))
         if self.sep_bias is not None:
             y = y + self.sep_bias
 